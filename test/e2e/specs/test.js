02433be680a12071769e06f9d99afddfee2ce866
Maxence POUTORD <maxence.poutord@gmail.com>
2017-05-17 14:36:40 +0100

    :green_heart: fix CI

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -2,7 +2,7 @@
 // http://nightwatchjs.org/guide#usage
 
 module.exports = {
-  'default e2e tests': function (browser) {
+  'default e2e pickaday': function (browser) {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
@@ -11,9 +11,7 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.date-picker')
       .end()
   }
 }
